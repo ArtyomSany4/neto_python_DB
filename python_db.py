b2cf8dc73ed6686761f41b90a8b26e94c34392cb
IGNATEV_AA <Artyom-sany4@mail.ru>
2023-06-05 21:14:33 +0300

    1

--- python_db.py
+++ python_db.py
@@ -19,7 +19,8 @@ class DataBase:
             conn.commit()
         
         conn.close()        
-        
+
+# 1. Функция, создающая структуру БД (таблицы). 
     def create_table(self):
         SQL_query = """
         CREATE TABLE IF NOT EXISTS method_tst(
@@ -30,9 +31,19 @@ class DataBase:
             );"""
         return self.cursor(SQL_query)
         
+# 2.  Функция, позволяющая добавить нового клиента
+    def add_client(self):
+        name = input('Enter client\'s name: ')
+        surname = input('Enter client\'s surname: ')
+        email = input('Enter client\'s email: ')
+        SQL_query = (
+            f'INSERT INTO method_tst(name, surname, email) VALUES'
+            f'(\'{name}\', \'{surname}\', \'{email}\');'
+            )
+        return self.cursor(SQL_query)
         
-        
+    
         
 # tbl_name = input('Enter table name: ')
 test_exemplyar = DataBase()
-test_exemplyar.create_table()
+test_exemplyar.add_client()
