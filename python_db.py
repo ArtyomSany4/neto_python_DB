52956a0b262ff00740cc8920ea221ed4802e21ce
IGNATEV_AA <Artyom-sany4@mail.ru>
2023-06-05 18:39:07 +0300

    Инит получает пасс о БД, Курсор подготавливает курсор, криэйт пушит в курсор текст запроса. Работает

--- python_db.py
+++ python_db.py
@@ -1,35 +1,38 @@
 import psycopg2
 
 
-
 class DataBase:
     def __init__(self):
         with open('token.txt', 'r') as token_file:
             db_password = token_file.read().strip()
         self.password = db_password
+        # self.password = 'МЕСТО ПОД ПАРОЛЬ'
 
-    def create_table(self, tbl_name):
+    def cursor(self, SQL_query):
         conn = psycopg2.connect(
             database='Python_DB', 
             user='postgres', 
             password=self.password
             )
         with conn.cursor() as cur:
-            cur.execute(f'CREATE TABLE IF NOT EXISTS {tbl_name}(id SERIAL PRIMARY KEY);')
+            cur.execute(SQL_query)
             conn.commit()
         
-        conn.close()
+        conn.close()        
         
-tbl_name = input('Enter table name: ')
+    def create_table(self):
+        SQL_query = """
+        CREATE TABLE IF NOT EXISTS method_tst(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255),
+            surname VARCHAR(255),
+            email VARCHAR(255)
+            );"""
+        return self.cursor(SQL_query)
+        
+        
+        
+        
+# tbl_name = input('Enter table name: ')
 test_exemplyar = DataBase()
-test_exemplyar.create_table(tbl_name)
-
-
-    # def __init__(self, token: str):
-    #     self.token = token
-
-    # def get_headers(self):
-    #     return {
-    #         'Content-Type': 'application/json',
-    #         'Authorization': 'OAuth {}'.format(self.token)
-    #     }
+test_exemplyar.create_table()
