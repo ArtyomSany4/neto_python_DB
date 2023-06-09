a25448901662a1b819a214a25266dad4d600f1c7
IGNATEV_AA <Artyom-sany4@mail.ru>
2023-06-09 23:43:57 +0300

    Готовы 6 функций. Работают. Необходима оптимизация на повторяющиеся элементы. Селекты переписать под рекомендации из урока. Дописать последюю фнункцию

--- python_db.py
+++ python_db.py
@@ -20,10 +20,9 @@ class DataBase:
         conn.close()        
 
     
-    def clnt_srch_query(self, client_id):
+    def select_meth(self, select):
         # client_id = input('Enter client_id: ')
-        client_id = client_id
-        select = f'SELECT name FROM clients WHERE id = {client_id}'
+        select = select 
         conn = psycopg2.connect(
             database='Python_DB', 
             user='postgres', 
@@ -47,7 +46,7 @@ class DataBase:
             );
         CREATE TABLE IF NOT EXISTS phone_numbers(
             id SERIAL PRIMARY KEY,
-            phone_number INTEGER UNIQUE,
+            phone_number BIGINT UNIQUE,
             client_id INTEGER REFERENCES clients(id)
             );
         
@@ -70,27 +69,122 @@ class DataBase:
 # 3. Функция, позволяющая добавить телефон для существующего клиента.
     def add_clnt_phone_nmbr(self):
         client_id = input('Enter client_id: ')
-        resp = self.clnt_srch_query(client_id)
-        if resp != None:
+        select = f'SELECT name FROM clients WHERE id = {client_id}'
+        resp = self.select_meth(select)
+        if resp == None:
+            print(f'Client with client_id = {client_id} not found. Enter a correct client_id')
+        else:
             phone_number = input('Enter client phone_number: ')
             SQL_query = (
                 f'INSERT INTO phone_numbers(client_id, phone_number) VALUES'
                 f'(\'{client_id}\', \'{phone_number}\');'
                 )
             self.cursor(SQL_query)
-        print(f'Номер телефона {phone_number} успешно добавлен клиенту с ИД {client_id}!')
+            print(f'Номер телефона {phone_number} успешно добавлен клиенту с ИД {client_id}!')
+
+# 4.  Функция, позволяющая изменить данные о клиенте
+    def updt_clnt_data(self):
+        route = input("""
+What do you want to change?
+Enter your choice: 
+1 - name, 2 - surname, 3 - email.
+            """)
+        if int(route) == 1:
+            client_id = input('Enter client_id: ') 
+            select = f'SELECT name FROM clients WHERE id = {client_id}'
+            if self.select_meth(select) == None:
+                print(f'Client with client_id = {client_id} not found. Enter a correct client_id')
+            else:
+                name = input('Enter client\'s new name: ')
+                updt = (
+                    f'UPDATE clients\n'
+                    f'SET name = \'{name}\'\n'
+                    f'WHERE id = {client_id};'
+                    )
+                
+        if int(route) == 2:
+            client_id = input('Enter client_id: ') 
+            select = f'SELECT name FROM clients WHERE id = {client_id}'
+            if self.select_meth(select) == None:
+                print(f'Client with client_id = {client_id} not found. Enter a correct client_id')
+            else:
+                surname = input('Enter client\'s new surname: ')
+                updt = (
+                    f'UPDATE clients\n'
+                    f'SET name = \'{surname}\'\n'
+                    f'WHERE id = {client_id};'
+                    )  
+
+        if int(route) == 3:
+            client_id = input('Enter client_id: ') 
+            select = f'SELECT name FROM clients WHERE id = {client_id}'
+            if self.select_meth(select) == None:
+                print(f'Client with client_id = {client_id} not found. Enter a correct client_id')
+            else:
+                email = input('Enter client\'s new email: ')
+                updt = (
+                    f'UPDATE clients\n'
+                    f'SET email = \'{email}\'\n'
+                    f'WHERE id = {client_id};'
+                    )
+        self.cursor(updt)
+        return print('Data was updated successfully.')
+
+# 5. Функция, позволяющая удалить телефон для существующего клиента.
+    def phone_del(self):
+        client_id = input('Enter client_id: ') 
+        select = f'SELECT name FROM clients WHERE id = {client_id}'
+        if self.select_meth(select) == None:
+            print(f'Client with client_id = {client_id} not found. Enter a correct client_id')
+        else:
+            delete = (
+                    f'DELETE from phone_numbers\n'
+                    f'WHERE client_id = {client_id};'
+                )
+        self.cursor(delete)
+        return print('Phone number successfully deleted.')
 
 
 
+# 6. Функция, позволяющая удалить существующего клиента.
+    def client_del(self):
+        client_id = input('Enter client_id: ') 
+        select = f'SELECT name FROM clients WHERE id = {client_id}'
+        if self.select_meth(select) == None:
+            print(f'Client with client_id = {client_id} not found. Enter a correct client_id')
+        else:
+            select = f'SELECT * FROM phone_numbers WHERE client_id = {client_id}'
+            delete = (
+                    f'DELETE from clients\n'
+                    f'WHERE id = {client_id};'
+                )
+            if self.select_meth(select) == None:
+                self.cursor(delete)
+            else:
+                self.phone_del()
+                self.cursor(delete)              
+        return print('Client successfully deleted.')
 
+# 7. Функция, позволяющая найти клиента по его данным: имени, фамилии, email или телефону.
+    def multy_select(self, name = '*', surname = '*', email = '*', phone_number = '*'):
+        
+        
 
-
-     
-# tbl_name = input('Enter table name: ')
 test_exemplyar = DataBase()
-
-# test_exemplyar.add_client()
-test_exemplyar.add_clnt_phone_nmbr()
-# test_exemplyar.clnt_search_query()
-# test_exemplyar.add_clnt_phone_numb()
+# 1. Функция, создающая структуру БД (таблицы).        
 # test_exemplyar.create_table()
+
+# 2.  Функция, позволяющая добавить нового клиента
+# test_exemplyar.add_client()
+
+# 3. Функция, позволяющая добавить телефон для существующего клиента.
+# test_exemplyar.add_clnt_phone_numb()
+
+# 4.  Функция, позволяющая изменить данные о клиенте
+# test_exemplyar.updt_clnt_data()
+
+# 5. Функция, позволяющая удалить телефон для существующего клиента.
+# test_exemplyar.phone_del()
+
+# 6. Функция, позволяющая удалить существующего клиента.
+test_exemplyar.client_del()
